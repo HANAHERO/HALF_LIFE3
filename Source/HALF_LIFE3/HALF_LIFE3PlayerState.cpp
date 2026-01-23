9c95edc523168c0bf6a841ebf45e8270164a56f1
HANAHERO <2104136460@qq.com>
2026-01-24 00:55:42 +0800

    优化蹲下动画，死亡显示结算画面，自动重开，游戏整体可玩

---
+++ Source/HALF_LIFE3/HALF_LIFE3PlayerState.cpp
@@ -0,0 +1,9 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "HALF_LIFE3PlayerState.h"
+
+void AddScore()
+{
+	
+}
---
+++ Source/HALF_LIFE3/HALF_LIFE3PlayerState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "HALF_LIFE3PlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HALF_LIFE3_API AHALF_LIFE3PlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+	
+
+	
+};
